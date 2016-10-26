cf7ae801e0244b48e455227d254a8ca42b3b7491
Silvana Goberdhan-Vigle <silvanagoberdhanvigle@gmail.com>
2016-10-26 11:01:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 function getIssues() {
+
+  fetch('/javascript-fetch-lab\/issues', {
+    method: 'get'
+
+  }).then(res => console.log(res));
 }
 
 function showIssues(json) {
 }
 
 function createIssue() {
+  const token = 'YOUR_TOKEN_HERE';
+  const postData = {
+    body: 'test body'
+  };
+
+  fetch('/javascript-fetch-lab\/issues', {
+    method: 'post',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => console.log(res));
 }
 
 function showResults(json) {
@@ -13,6 +30,15 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  const token = 'YOUR_TOKEN_HERE';
+
+
+  fetch('https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab', {
+    method: 'post',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => console.log(res));
 }
 
 function getToken() {
